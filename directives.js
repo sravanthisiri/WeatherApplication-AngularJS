f1fb132000e2d32ee95351eebff8c7049b8a7307
Sai Charan Reddy Patlolla <scrpatlolla@charan.home>
2016-12-18 14:09:47 -0500

    Added the custom directive

--- directives.js
+++ directives.js
@@ -1,11 +1,6 @@
 weatherApp.directive('weatherApplication', function() {
     return {
         restrict: 'AECM',
-        templateUrl:'template.html',
-        replace: true,
-        scope: {
-            
-        }
-        
+        templateUrl:'template.html'
     }
 });
